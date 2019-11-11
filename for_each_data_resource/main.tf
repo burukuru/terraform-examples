79a6c1b88237751f155a558b3edef6d7c167ffb2
Thanh C Pham <burukuru@gmail.com>
2019-11-11 22:28:15 +0000

    Add example for_each with data and resource.

---
+++ for_each_data_resource/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+variable "amis" {
+  type = "map"
+  default = {
+    "amazon" = "amzn2-ami-hvm-2.0.20191024.3-x86_64-gp2"                         # ami-040ba9174949f6de4
+    "ubuntu" = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20191002" # ami-02df9ea15c1778c9c
+  }
+}
+
+data "aws_ami" "this" {
+  for_each = var.amis
+
+  owners      = ["amazon", "099720109477"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = [each.value]
+  }
+}
+
+resource "aws_instance" "ec2" {
+  for_each = data.aws_ami.this
+
+  instance_type = "t3.micro"
+  ami           = each.value.image_id
+}
